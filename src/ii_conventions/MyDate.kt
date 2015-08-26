969a85c252086f9b4dacd25205d61071b54d0fb6
Jachu <jazuma5@gmail.com>
2015-08-26 22:04:36 +0200

    Koan 11 finished

--- src/ii_conventions/MyDate.kt
+++ src/ii_conventions/MyDate.kt
@@ -1,6 +1,33 @@
 package ii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import syntax.qualifiedThis.labelsForExtensionFunctionLiterals
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate>{
+    override fun compareTo(other: MyDate): Int {
+
+        val days_date1 = computeDays(year,month,dayOfMonth);
+        val days_date2 = computeDays(other.year,other.month,other.dayOfMonth);
+        val difference = days_date1-days_date2;
+
+        if(difference == 0){
+            return 0
+        }
+        if(difference >0){
+            return 1
+        }
+
+        return -1
+
+
+    }
+
+    fun computeDays(year: Int, month: Int, dayOfMonth: Int):Int{
+        val DAYS_YEAR = 365
+        val DAYS_MONTH = 31
+        return year*DAYS_YEAR+month*DAYS_MONTH+dayOfMonth
+
+    }
+}
 
 enum class TimeInterval {
     DAY,
