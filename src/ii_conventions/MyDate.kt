0b199de00f2416c59e40ce4af7a8002004e7d1b4
Jachu <jazuma5@gmail.com>
2015-08-26 22:06:38 +0200

    Added comments

--- src/ii_conventions/MyDate.kt
+++ src/ii_conventions/MyDate.kt
@@ -4,7 +4,8 @@ import syntax.qualifiedThis.labelsForExtensionFunctionLiterals
 
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate>{
     override fun compareTo(other: MyDate): Int {
-
+        // This solution ignores leap years and 28 and 30 days month,
+        // but it happens in both cases so the error is present in both cases
         val days_date1 = computeDays(year,month,dayOfMonth);
         val days_date2 = computeDays(other.year,other.month,other.dayOfMonth);
         val difference = days_date1-days_date2;
